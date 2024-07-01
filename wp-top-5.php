ac49bfc07044b383e454c92a440ea1ead36b56d5
James Wilson <jwilson529@hotmail.com>
2024-07-01 14:07:20 -0500

    verion bump

--- wp-top-5.php
+++ wp-top-5.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WP Top 5
  * Plugin URI:        https://oneclickcontent.com
  * Description:       WP Top 5 - Generate Key Takeaways with AI
- * Version:           1.1.0
+ * Version:           1.1.2
  * Author:            James Wilson
  * Author URI:        https://oneclickcontent.com/
  * License:           GPL-2.0+
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WP_TOP_5_VERSION', '1.1.0' );
+define( 'WP_TOP_5_VERSION', '1.1.2' );
 
 /**
  * The code that runs during plugin activation.
