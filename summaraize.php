3728a5d5ffd3b1f5ed551f3d79789be78ef5ef85
James Wilson <jwilson529@hotmail.com>
2024-08-10 11:04:06 -0500

    Adjusting auto save to only use predefined options

--- summaraize.php
+++ summaraize.php
@@ -15,7 +15,7 @@
  * Plugin Name:       SummarAIze
  * Plugin URI:        https://oneclickcontent.com
  * Description:       SummarAIze - Generate Key Takeaways with AI
- * Version:           1.1.4
+ * Version:           1.1.5
  * Author:            James Wilson
  * Author URI:        https://oneclickcontent.com/
  * License:           GPL-2.0+
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SUMMARAIZE_VERSION', '1.1.4' );
+define( 'SUMMARAIZE_VERSION', '1.1.5' );
 
 /**
  * The code that runs during plugin activation.
