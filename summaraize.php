a741ec3a524c5b47b1e4e25771154231fc548681
James Wilson <jwilson529@hotmail.com>
2024-09-14 08:15:12 -0500

    Updating save function

--- summaraize.php
+++ summaraize.php
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SUMMARAIZE_VERSION', '1.1.11' );
+define( 'SUMMARAIZE_VERSION', '1.1.12' );
 
 /**
  * The code that runs during plugin activation.
